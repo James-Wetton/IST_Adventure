adc6699b91240194bcadc1959e00fcb54f59e999
Winston <winston_tran@outlook.com>
2018-06-13 11:11:17 +1000

    Variable System included

--- main.js
+++ main.js
@@ -7,3 +7,12 @@ function Change(x){
 function Test(){
 	alert("Working")
 }
+function Add(name, obj){
+	var arr = JSON.parse(window.name);
+	arr[name] = obj;
+	window.name = JSON.stringify(arr);
+}
+function Get(name){
+	var arr = JSON.parse(window.name);
+	return arr[name];
+}
